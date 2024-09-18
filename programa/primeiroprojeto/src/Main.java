88836641768373b8826492c084f594d82a4d37da
fidelisluc4sm <luckas_jf@hotmail.com>
2024-09-18 10:11:54 -0300

    1.0.2 Java: trabalhar com variáveis

--- programa/primeiroprojeto/src/Main.java
+++ programa/primeiroprojeto/src/Main.java
@@ -2,11 +2,29 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Olá Mundo");
+        System.out.println("Olá Mundo");
+        System.out.println("Filme: Top Gun: Maverick");
 
-        for (int i = 1; i <= 5; i++) {
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: "+anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilem = 8.1;
 
-            System.out.println("i = " + i);
-        }
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = ("""
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """+anoDeLancamento);
+        System.out.println(sinopse);
+
+        int classificacao;
+        classificacao = (int) (media / 2);
+        System.out.println(classificacao);
+
+
     }
 }
