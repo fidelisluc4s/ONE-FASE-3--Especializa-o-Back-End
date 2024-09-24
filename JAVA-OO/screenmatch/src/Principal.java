8e83a849152046b5dd016f4726bfbf2fd051cf12
fidelisluc4sm <luckas_jf@hotmail.com>
2024-09-24 14:24:24 -0300

    1.1.2 exercicio01

--- JAVA-OO/screenmatch/src/Principal.java
+++ JAVA-OO/screenmatch/src/Principal.java
@@ -1,17 +1,25 @@
 public class Principal {
+//    public static void main(String[] args) {
+//        Filme meuFilme = new Filme();
+//
+//        meuFilme.nome = "O poderoso Chefão";
+//        meuFilme.anoDeLancamento = 2017;
+//        meuFilme.duracaoEmMinutos = 180;
+//
+//        meuFilme.exibirFichaTecnica();
+//        meuFilme.avalia(8);
+//        meuFilme.avalia(7);
+//        meuFilme.avalia(10);
+//        System.out.println(meuFilme.somaDeAvaliacoes);
+//        System.out.println(meuFilme.totalDeAvaliacoes);
+//        System.out.println(String.format("%.2f",meuFilme.retornarMedia()));
+//    }
+
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
 
-        meuFilme.nome = "O poderoso Chefão";
-        meuFilme.anoDeLancamento = 2017;
-        meuFilme.duracaoEmMinutos = 180;
-
-        meuFilme.exibirFichaTecnica();
-        meuFilme.avalia(8);
-        meuFilme.avalia(7);
-        meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDeAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
-        System.out.println(String.format("%.2f",meuFilme.retornarMedia()));
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "Lucas";
+        pessoa1.idade = 32;
+        pessoa1.olaMundo();
     }
 }
