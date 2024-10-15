02a4f372ced3b2ea45b5a2d1ade4751424297fb4
fidelisluc4sm <luckas_jf@hotmail.com>
2024-10-15 02:40:03 -0300

    1.2.0 lista ligada || estrutura de dados

---
+++ exercicios/ExercicioEstruturaListaLigadas/src/br/com/alura/exercicioestruturalistaligada/Celula.java
@@ -0,0 +1,24 @@
+package br.com.alura.exercicioestruturalistaligada;
+
+public class Celula {
+
+    private final Object elemento;
+    private Celula proximo;
+
+    public Celula(Object elemento, Celula proximo) {
+        this.elemento = elemento;
+        this.proximo = proximo;
+    }
+
+    public Celula getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(Celula proximo) {
+        this.proximo = proximo;
+    }
+
+    public Object getElemento() {
+        return elemento;
+    }
+}
