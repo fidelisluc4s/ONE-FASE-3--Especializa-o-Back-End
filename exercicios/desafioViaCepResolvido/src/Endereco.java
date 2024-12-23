d38b74de999444349084c2f1355cd156558aa82c
fidelisluc4sm <luckas_jf@hotmail.com>
2024-12-23 11:52:47 -0300

    1.4.0 desafio viacep | Json | Gson | HTTPS ConsultaCep| FileWrite GeradorDeArquivo

---
+++ exercicios/desafioViaCepResolvido/src/Endereco.java
@@ -0,0 +1,7 @@
+public record Endereco(String logradouro,
+                       String cep,
+                       String localidade,
+                       String complemento,
+                       String uf)
+{
+}
