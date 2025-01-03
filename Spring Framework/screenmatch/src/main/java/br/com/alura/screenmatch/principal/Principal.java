ee78b4364d8132d6c52495d3ccaa6d262e15cde0
fidelisluc4sm <luckas_jf@hotmail.com>
2025-01-03 02:21:30 -0300

    1.5.0 introdução de lambda e operações com stream

--- Spring Framework/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ Spring Framework/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -7,6 +7,7 @@ import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,21 +19,21 @@ public class Principal {
     private ConverteDados conversor = new ConverteDados();
 
     public void exibeMenu(){
-        System.out.println("Digite o nome da série para busca");
-        var nomeSerie = leitura.nextLine();
-        var json = cosumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
-
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dados.totalTemporadas(); i++){
-			json = cosumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season="+ i + API_KEY);
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
+//        System.out.println("Digite o nome da série para busca");
+//        var nomeSerie = leitura.nextLine();
+//        var json = cosumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+//        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+//        System.out.println(dados);
+//
+//
+//		List<DadosTemporada> temporadas = new ArrayList<>();
+//
+//		for (int i = 1; i <= dados.totalTemporadas(); i++){
+//			json = cosumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season="+ i + API_KEY);
+//			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+//			temporadas.add(dadosTemporada);
+//		}
+//		temporadas.forEach(System.out::println);
 //
 //        for (int i = 0; i < dados.totalTemporadas(); i++){
 //            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
@@ -41,7 +42,17 @@ public class Principal {
 //            }
 //        }
         //lambda
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
-        temporadas.forEach(System.out::println);
+//        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+//        temporadas.forEach(System.out::println);
+
+        //operação intermediaria
+        List<String> nomes = Arrays.asList("Lucas", "Paulo", "Rodrigo", "Luan", "Miguel");
+
+        nomes.stream()//sempre chama
+                .sorted()// ordena
+                .limit(3)//limitar pessoas
+                .filter(n -> n.startsWith("M"))//o nome começa com M
+                .map(n -> n.toUpperCase())// colocar o nome com letra maiusculas
+                .forEach(System.out::println);//imprimir os dados | operaçoes finais
     }
 }
